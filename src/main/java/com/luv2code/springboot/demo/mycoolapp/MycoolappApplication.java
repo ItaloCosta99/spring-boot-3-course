92c8764c5026e08f82679500695559c02820217a
Italo Costa <italocostajobs@gmail.com>
2023-06-30 17:09:00 -0300

    Component Scanning Part 2

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,7 +3,9 @@ package com.luv2code.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {"com.luv2code.springboot.demo.mycoolapp", "com.luv2code.util"}
+)
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/luv2code/util/Coach.java
+++ src/main/java/com/luv2code/util/Coach.java
@@ -1,4 +1,4 @@
-package com.luv2code.springboot.demo.mycoolapp.common;
+package com.luv2code.util;
 
 public interface Coach {
     String getDailyWorkout();
