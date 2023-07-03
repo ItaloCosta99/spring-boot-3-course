0dd8af79ddfb74966e31c489950ba78a86b4c2cf
Italo Costa <italocostajobs@gmail.com>
2023-07-03 15:58:29 -0300

    Learning about Beans

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import com.luv2code.springboot.demo.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ public class DemoController {
 
     // Define a constructor for dependency injection
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("aquatic") Coach theCoach) {
+        System.out.println("In constructor" + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
