b219f26986c045e95b22dcafdff5d4345b292ea8
Italo Costa <italocostajobs@gmail.com>
2023-07-10 14:23:47 -0300

    CRUD with JpaRepository

--- src/main/java/com/luv2code/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/demo/service/EmployeeServiceImpl.java
@@ -1,44 +1,49 @@
 package com.luv2code.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.luv2code.demo.dao.EmployeeDAO;
+import com.luv2code.demo.dao.EmployeeRepository;
 import com.luv2code.demo.entity.Employee;
-
-import jakarta.transaction.Transactional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-  private EmployeeDAO employeeDAO;
+  private EmployeeRepository employeeRepository;
 
   @Autowired
-  public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
-    employeeDAO = theEmployeeDAO;
+  public EmployeeServiceImpl(EmployeeRepository theEmployeeDAO) {
+    employeeRepository = theEmployeeDAO;
   }
 
   @Override
   public List<Employee> findAll() {
-    return employeeDAO.findAll();
+    return employeeRepository.findAll();
   }
 
   @Override
   public Employee findById(int theId) {
-    return employeeDAO.findById(theId);
+    Optional<Employee> result = employeeRepository.findById(theId);
+    Employee theEmployee = null;
+    if (result.isPresent()) {
+      theEmployee = result.get();
+    } else {
+      // we didn't find the employee
+      throw new RuntimeException("Did not find employee id - " + theId);
+    }
+    return theEmployee;
   }
 
-  @Transactional
   @Override
   public Employee save(Employee theEmployee) {
-    return employeeDAO.save(theEmployee);
+    return employeeRepository.save(theEmployee);
   }
 
-  @Transactional
   @Override
   public void deleteById(int theId) {
-    employeeDAO.deleteById(theId);
+    employeeRepository.deleteById(theId);
   }
 
 }
