64f8457c0943c97f0c3d483e74e66916078321f7
Italo Costa <italocostajobs@gmail.com>
2023-07-10 16:04:14 -0300

    Spring Data REST

--- src/main/java/com/luv2code/demo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/demo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.demo.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
   
 }
