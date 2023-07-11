9a68b381f7c2ce8717f0ac6da2498e243553ccca
Italo Costa <italocostajobs@gmail.com>
2023-07-11 12:55:18 -0300

    Preparing for Thymeleaf

--- src/main/java/com/luv2code/demo/dao/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.luv2code.demo.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-  
-}
